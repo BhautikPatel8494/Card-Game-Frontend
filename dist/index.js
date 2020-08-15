27bdfcf3dd7f2adcc641bd88168d1cdcfb6ea1bb
Bhautik <bhautikpatel8494@gmail.com>
2020-08-15 23:08:45 +0530

    New Changes

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "MemoryGame", {
+  enumerable: true,
+  get: function get() {
+    return _game.MemoryGame;
+  }
+});
+
+var _game = require("../game");
---
+++ src/component/index.js
@@ -0,0 +1,5 @@
+import { MemoryGame } from '../game';
+
+export { 
+    MemoryGame
+};
